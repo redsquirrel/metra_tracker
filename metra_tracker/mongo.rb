6470780301db40cc66c8a24e25e56129523e6fb8
Dave Hoover <dave.hoover@gmail.com>
2010-12-27 08:40:17 -0600

    Fixing Mongo

--- metra_tracker/mongo.rb
+++ metra_tracker/mongo.rb
@@ -6,7 +6,7 @@ module MetraTracker
   module Mongo
     def self.store(params)
       uri = URI.parse(ENV['MONGOHQ_URL'])
-      conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+      conn = ::Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
       db = conn.db(uri.path.gsub(/^\//, ''))
       requests = db.collection('requests')
       requests.insert(params.merge("created_at" => Time.now))
